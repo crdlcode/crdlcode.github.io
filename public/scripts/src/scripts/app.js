5d915f21c1e6754ec923d7110b58ba39c4391aa0
Curtis C <curtis.chat.hk@gmail.com>
2018-12-12 18:47:30 -0500

    cursor

--- public/scripts/src/scripts/app.js
+++ public/scripts/src/scripts/app.js
@@ -58,6 +58,16 @@ const init = () => {
   reveal();
 };
 
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+  typeSpeed: 40
+}
+
+var typed = new Typed(".element", options);
+
 // Doc ready
 $(function() {
   init();
